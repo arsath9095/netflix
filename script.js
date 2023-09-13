d9e5d22e7e3e2ddb7fadbb11a5ccb60a62070fff
arsath9095 <141390370+arsath9095@users.noreply.github.com>
2023-09-13 12:38:54 +0530

    Add files via upload
    files commit

---
+++ script.js
@@ -0,0 +1,9 @@
+/*var Content =document.querySelector(".content");
+
+function ToggleClick(){
+    Content.classList.toggle("content-height");
+
+
+}*/
+document.querySelector('input[name="acordation"]:checked').checked=false;
+
